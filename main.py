b55a478359d9838a2dea1afb12faf2a8bae26811
D4rkfrightz <meyousaf2020@gmail.com>
2025-09-04 19:34:59 +0500

    Backend python

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    ItemNumber: int
+
+
+app = FastAPI()
+result = []
+
+@app.post("/items/")
+async def create_item(item: Item):
+    global result
+    result.append(item.__dict__)
+    return {"message":"successfully Data insert"}
+
+
+@app.get("/items/")
+async def get_items():
+    global result
+    return result
+
+
+@app.delete("/item/")
+async def delete_item(ItemNumber: int):
+    global result
+    for item in result:
+        if (item['ItemNumber'] == ItemNumber):
+            result.remove(item)
+    return {"message":"successfully Data Deleted"}
+
+
+@app.patch("/item/")
+async def update_item(saad: Item):
+    global result
+    for item in range(0,len(result)):
+        if (result[item]['ItemNumber'] == saad.__dict__['ItemNumber']):
+            result[item] = saad.__dict__
+    return {"message":"successfully Data updated"}
